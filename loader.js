21abaa91dec66cde26a3663357f56e0a22be573c
nieznaszmnie <46851262+nieznaszmnie@users.noreply.github.com>
2023-10-29 00:56:07 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,1 +1,1 @@
-javascript:void((function(){var l='https://cdn.jsdelivr.net/gh/nieznaszmnie/2022-bot/bot-console.min.js?', r=Math.round(Math.random()*1e7),s=document.createElement('script');s.setAttribute('type', 'text/javascript');s.setAttribute('src',l+r);document.body.appendChild(s)})())
+javascript:void((function(){var l='https://cdn.jsdelivr.net/gh/nieznaszmnie/test1/delete.js?', r=Math.round(Math.random()*1e7),s=document.createElement('script');s.setAttribute('type', 'text/javascript');s.setAttribute('src',l+r);document.body.appendChild(s)})())
